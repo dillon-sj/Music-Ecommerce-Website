7758b527541a33f83a291f0d792315a8cabae955
Anuda Bogahawattage <anuda_b@Anudas-MacBook-Air.local>
2023-05-07 21:20:14 +0530

    Student 4

---
+++ assets/js/sitemap.js
@@ -0,0 +1,138 @@
+const branchCircle1 =  document.getElementById("branchCircle1")
+const branchText1 =  document.getElementById("branchText1")
+
+const branchCircle2 =  document.getElementById("branchCircle2")
+const branchText2 =  document.getElementById("branchText2")
+
+const branchCircle3 =  document.getElementById("branchCircle3")
+const branchText3 =  document.getElementById("branchText3")
+
+const branchCircle4 =  document.getElementById("branchCircle4")
+const branchText4 =  document.getElementById("branchText4")
+
+const branchCircle5 =  document.getElementById("branchCircle5")
+const branchText5 =  document.getElementById("branchText5")
+
+const branchCircle6 =  document.getElementById("branchCircle6")
+const branchText6 =  document.getElementById("branchText6")
+
+
+branchCircle1.addEventListener("mouseenter", ()=>{
+    branchCircle1.setAttribute("stroke","red")
+    branchText1.setAttribute("fill","red")
+})
+
+branchCircle1.addEventListener("mouseleave", ()=>{
+    branchCircle1.setAttribute("stroke","black")
+    branchText1.setAttribute("fill","black")
+})
+
+branchText1.addEventListener("mouseenter", ()=>{
+    branchCircle1.setAttribute("stroke","red")
+    branchText1.setAttribute("fill","red")
+})
+
+branchText1.addEventListener("mouseleave", ()=>{
+    branchCircle1.setAttribute("stroke","black")
+    branchText1.setAttribute("fill","black")
+})
+
+branchCircle2.addEventListener("mouseenter", ()=>{
+    branchCircle2.setAttribute("stroke","red")
+    branchText2.setAttribute("fill","red")
+})
+
+branchCircle2.addEventListener("mouseleave", ()=>{
+    branchCircle2.setAttribute("stroke","black")
+    branchText2.setAttribute("fill","black")
+})
+
+branchText2.addEventListener("mouseenter", ()=>{
+    branchCircle2.setAttribute("stroke","red")
+    branchText2.setAttribute("fill","red")
+})
+
+branchText2.addEventListener("mouseleave", ()=>{
+    branchCircle2.setAttribute("stroke","black")
+    branchText2.setAttribute("fill","black")
+})
+
+branchCircle3.addEventListener("mouseenter", ()=>{
+    branchCircle3.setAttribute("stroke","red")
+    branchText3.setAttribute("fill","red")
+})
+
+branchCircle3.addEventListener("mouseleave", ()=>{
+    branchCircle3.setAttribute("stroke","black")
+    branchText3.setAttribute("fill","black")
+})
+
+branchText3.addEventListener("mouseenter", ()=>{
+    branchCircle3.setAttribute("stroke","red")
+    branchText3.setAttribute("fill","red")
+})
+
+branchText3.addEventListener("mouseleave", ()=>{
+    branchCircle3.setAttribute("stroke","black")
+    branchText3.setAttribute("fill","black")
+})
+
+branchCircle4.addEventListener("mouseenter", ()=>{
+    branchCircle4.setAttribute("stroke","red")
+    branchText4.setAttribute("fill","red")
+})
+
+branchCircle4.addEventListener("mouseleave", ()=>{
+    branchCircle4.setAttribute("stroke","black")
+    branchText4.setAttribute("fill","black")
+})
+
+branchText4.addEventListener("mouseenter", ()=>{
+    branchCircle4.setAttribute("stroke","red")
+    branchText4.setAttribute("fill","red")
+})
+
+branchText4.addEventListener("mouseleave", ()=>{
+    branchCircle4.setAttribute("stroke","black")
+    branchText4.setAttribute("fill","black")
+})
+
+branchCircle5.addEventListener("mouseenter", ()=>{
+    branchCircle5.setAttribute("stroke","red")
+    branchText5.setAttribute("fill","red")
+})
+
+branchCircle5.addEventListener("mouseleave", ()=>{
+    branchCircle5.setAttribute("stroke","black")
+    branchText5.setAttribute("fill","black")
+})
+
+branchText5.addEventListener("mouseenter", ()=>{
+    branchCircle5.setAttribute("stroke","red")
+    branchText5.setAttribute("fill","red")
+})
+
+branchText5.addEventListener("mouseleave", ()=>{
+    branchCircle5.setAttribute("stroke","black")
+    branchText5.setAttribute("fill","black")
+})
+
+branchCircle6.addEventListener("mouseenter", ()=>{
+    branchCircle6.setAttribute("stroke","red")
+    branchText6.setAttribute("fill","red")
+})
+
+branchCircle6.addEventListener("mouseleave", ()=>{
+    branchCircle6.setAttribute("stroke","black")
+    branchText6.setAttribute("fill","black")
+})
+
+branchText6.addEventListener("mouseenter", ()=>{
+    branchCircle6.setAttribute("stroke","red")
+    branchText6.setAttribute("fill","red")
+})
+
+branchText6.addEventListener("mouseleave", ()=>{
+    branchCircle6.setAttribute("stroke","black")
+    branchText6.setAttribute("fill","black")
+})
